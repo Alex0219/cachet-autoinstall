54117f3b14c01faa29911f700865beb7cb46f227
Alexander <33397817+Alex0219@users.noreply.github.com>
2020-01-29 15:08:10 +0100

    Add files via upload

--- cachet-install.sh
+++ cachet-install.sh
@@ -26,7 +26,6 @@ mysql -uroot << EOF
 UPDATE mysql.user SET Password=PASSWORD('$randPassword') WHERE User='root';
 DELETE FROM mysql.user WHERE user='root' AND host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE user='';
-DROP DATABASE test;
 CREATE DATABASE cachet;
 GRANT ALL ON cachet.* TO 'cachetuser' IDENTIFIED BY '$randPassword';
 FLUSH PRIVILEGES;
